92549f45f96764c06658fa265c06a7515bbf703a
Aritra1704 <aritra1704@gmail.com>
2016-03-15 09:05:11 +0530

    Map, Service and front camera photo

--- app/src/main/java/com/example/arpaul/rideit/Common/AppConstant.java
+++ app/src/main/java/com/example/arpaul/rideit/Common/AppConstant.java
@@ -4,5 +4,5 @@ package com.example.arpaul.rideit.Common;
  * Created by ARPaul on 13-03-2016.
  */
 public class AppConstant {
-    public static final long REPEAT_TIME = 1000 * 60 * 2;
+    public static final long REPEAT_TIME = 1000 * 60 * 5;
 }
